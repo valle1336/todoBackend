9ae102861a4bebfd9cb6f8200c6a64e6cd7a3b70
valle1336 <7alexxi@gmail.com>
2024-06-06 21:36:53 +0200

    Entity created

--- src/main/java/com/valle1336/demo/Controllers/TaskController.java
+++ src/main/java/com/valle1336/demo/Controllers/TaskController.java
@@ -1,21 +1,22 @@
 package com.valle1336.demo.Controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/task")
 public class TaskController {
 
+    //Test
     @GetMapping("/hello")
     String hello() {
         return "Hello World";
     }
+    //Test
     @GetMapping("/secure")
     String secure() {
         return "Hello from Secure endpoint!!";
     }
+
 }
