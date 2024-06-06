70efcda0b38977aca9268c2858d973079ae64426
valle1336 <7alexxi@gmail.com>
2024-06-06 21:45:17 +0200

    Postmapping now works. Added repo, changes to TaskEntity.java and added @Postmapping in controller

--- src/main/java/com/valle1336/demo/Controllers/TaskController.java
+++ src/main/java/com/valle1336/demo/Controllers/TaskController.java
@@ -1,12 +1,25 @@
 package com.valle1336.demo.Controllers;
 
+import com.valle1336.demo.Entities.TaskEntity;
+import com.valle1336.demo.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/task")
 public class TaskController {
+
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+
 
     //Test
     @GetMapping("/hello")
@@ -17,6 +30,12 @@ public class TaskController {
     @GetMapping("/secure")
     String secure() {
         return "Hello from Secure endpoint!!";
+    }
+
+    @PostMapping("/create")
+    public TaskEntity createTask(@RequestBody TaskEntity taskEntity) {
+        taskEntity.setCreationDate(LocalDateTime.now());
+        return taskRepository.save(taskEntity);
     }
 
 }
--- src/main/java/com/valle1336/demo/Entities/TaskEntity.java
+++ src/main/java/com/valle1336/demo/Entities/TaskEntity.java
@@ -4,11 +4,13 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Setter
 @Table(name = "tasks")
-public class TodoEntity {
+public class TaskEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,15 +18,16 @@ public class TodoEntity {
     private String task;
     private boolean completed;
     private boolean prioritize;
+    private LocalDateTime creationDate;
 
-    public TodoEntity(Long id, String task, boolean completed, boolean prioritize) {
+    public TaskEntity(Long id, String task, boolean completed, boolean prioritize) {
         this.id = id;
         this.task = task;
         this.completed = completed;
         this.prioritize = prioritize;
     }
 
-    public TodoEntity() {
+    public TaskEntity() {
 
     }
 }
